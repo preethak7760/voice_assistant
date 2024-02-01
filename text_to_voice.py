ad2a09f9d61b9caf9cefbc1213612b148ad5b33b
preetha <preethak7760@gmail.com>
2024-02-01 21:08:55 +0530

    bug fixed

--- text_to_voice.py
+++ text_to_voice.py
@@ -1,7 +1,19 @@
 import pyttsx3
 
 def text_to_speech(text):
+    # Initialize the text-to-speech engine
     engine = pyttsx3.init()
+
+    # Set properties (optional)
+    # engine.setProperty('rate', 150)  # Speed of speech
+    # engine.setProperty('volume', 0.9)  # Volume level (0.0 to 1.0)
+
+    # Convert the text to speech
     engine.say(text)
+
+    # Wait for the speech to finish
     engine.runAndWait()
 
+
+if __name__=='__main__':
+    text_to_speech("Hello, how are you today?")
